c95a08d6c30adc4f03d70647ae5d04948902e81b
killswitch <razv888@gmail.com>
2023-08-13 00:44:57 +0300

    added the ability to read and write data into I/O ports, added the ability to find the postion of the coursor

---
+++ drivers/ports.h
@@ -0,0 +1,4 @@
+unsigned char io_port_byte_in (unsigned short port);
+unsigned char io_port_byte_out (unsigned short port, unsigned char data);
+unsigned short io_port_word_in (unsigned short port);
+unsigned short io_port_word_out (unsigned short port, unsigned char data);
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,24 @@
-void main(){
-    char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
+#include "../drivers/ports.h"  // Include the header file that likely defines functions for I/O port operations
+
+void main() {
+    // Output a byte (14) to I/O port 0x3d
+    io_port_byte_out(0x3d, 14);
+
+    // Read a byte from I/O port 0x3d5, then shift it left by 8 bits
+    int position = io_port_byte_in(0x3d5);
+    position = position << 8;
+
+    // Output a byte (15) to I/O port 0x3d4, then add the result of reading another byte from I/O port 0x3d5
+    io_port_byte_out(0x3d4, 15);
+    position += io_port_byte_in(0x3d5);
+
+    // Calculate an offset value from the 'position' variable, each unit represents two bytes
+    int offset_from_vga = position * 2;
+
+    // Set up a pointer to the VGA memory region (address 0xb8000), treating it as a character (byte) array
+    char* vga = (char*)0xb8000;
+
+    // Write 'X' to the VGA memory at the calculated offset (character), and set the color attribute (0x0f, white on black)
+    vga[offset_from_vga] = 'X';
+    vga[offset_from_vga + 1] = 0x0f;
 }
